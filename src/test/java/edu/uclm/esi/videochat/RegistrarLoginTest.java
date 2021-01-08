cf3178f818f56c5dfa3c3a4dc11b0e442594afc6
VICTOR <CLASE@LAPTOP-HK1DJLBJ>
2021-01-08 11:13:19 +0100

    Selenium comprueba el login

--- src/test/java/edu/uclm/esi/videochat/RegistrarLoginTest.java
+++ src/test/java/edu/uclm/esi/videochat/RegistrarLoginTest.java
@@ -39,65 +39,82 @@ import edu.uclm.esi.videochat.springdao.UserRepository;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class RegistrarLoginTest {
-  private WebDriver chrome;
-  private Map<String, Object> vars;
-  JavascriptExecutor js;
-  
-	
+	private WebDriver chrome;
+	private Map<String, Object> vars;
+	JavascriptExecutor js;
+
 	@Autowired
 	UserRepository usersRepo;
-  
-  @Before
-  public void setUp() {
-	 
-	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\CLASE\\Desktop\\TSW\\chromedriver.exe"); 
-	  
-    chrome = new ChromeDriver();
-    js = (JavascriptExecutor) chrome;
-    vars = new HashMap<String, Object>();
-    
-    
-  }
-  @After
-  public void tearDown() {
-    chrome.quit();
-  }
-  @Test
-  public void registrarLogin() {
-	  
-    usersRepo.delete(usersRepo.findByName("selenium97")); 
-    chrome.get("https://localhost:7500/?ojr=register");
-   
+
+	@Before
+	public void setUp() {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\CLASE\\Desktop\\TSW\\chromedriver.exe");
+
+		chrome = new ChromeDriver();
+		js = (JavascriptExecutor) chrome;
+		vars = new HashMap<String, Object>();
+
+	}
+
+	@After
+	public void tearDown() {
+		chrome.quit();
+	}
+
+	@Test
+	public void registrarLogin() {
+
+		usersRepo.delete(usersRepo.findByName("selenium97"));
+		chrome.get("https://localhost:7500/?ojr=register");
 
 		chrome.findElement(By.id("details-button")).click();
 		chrome.findElement(By.id("proceed-link")).click();
-		 chrome.manage().window().setSize(new Dimension(939, 1025));
+		chrome.manage().window().setSize(new Dimension(939, 1025));
+
+		WebElement cajaNombre = chrome
+				.findElement(By.xpath("//*[@id=\"globalBody\"]/oj-module/div[1]/div[2]/div/div/div/input[1]"));
+		WebElement cajaEmail = chrome
+				.findElement(By.xpath("//*[@id=\"globalBody\"]/oj-module/div[1]/div[2]/div/div/div/input[2]"));
+		WebElement cajaPwd1 = chrome
+				.findElement(By.xpath("//*[@id=\"globalBody\"]/oj-module/div[1]/div[2]/div/div/div/input[3]"));
+		WebElement cajaPwd2 = chrome
+				.findElement(By.xpath("//*[@id=\"globalBody\"]/oj-module/div[1]/div[2]/div/div/div/input[4]"));
+		// RemoteWebElement inputFile = (RemoteWebElement)
+		// chrome.findElement(By.xpath("//*[@id=\"globalBody\"]/oj-module/div[1]/div[2]/div/div/div/input[5]"));
+
+		cajaNombre.sendKeys("selenium97");
+		cajaEmail.sendKeys("selenium97@gmail.com");
+		cajaPwd1.sendKeys("123");
+		cajaPwd2.sendKeys("123");
+		chrome.findElement(By.xpath("/html/body/div/oj-module/div[1]/div[2]/div/div/div/button")).click();
+		WebElement botonCrearCuenta = chrome.findElement(By.id("btnCrearCuenta"));
+
+		chrome.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+		assertThat(chrome.switchTo().alert().getText(), is("Registrado correctamente"));
+		chrome.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		chrome.switchTo().alert().accept();
+
+//    chrome.findElement(By.cssSelector("html")).click();
+//    chrome.findElement(By.cssSelector(".oj-sm-12:nth-child(1) > input")).sendKeys("selenium97");
+//    chrome.findElement(By.cssSelector("button")).click();
+
+		WebElement loginNombre = chrome.findElement(
+				By.xpath("//*[@id=\"globalBody\"]/oj-module/div[1]/div[2]/div/div/div/div[1]/div[1]/input"));
+		WebElement loginPwd = chrome.findElement(
+				By.xpath("//*[@id=\"globalBody\"]/oj-module/div[1]/div[2]/div/div/div/div[1]/div[2]/input"));
+		WebElement btnEntrar = chrome.findElement(
+				By.xpath("//*[@id=\"globalBody\"]/oj-module/div[1]/div[2]/div/div/div/div[1]/div[3]/button"));
+
+		loginNombre.clear();
+		loginPwd.clear();
 		
+		loginNombre.sendKeys("selenium97");
+		loginPwd.sendKeys("123");
+		
+		chrome.findElement(By.xpath("//*[@id=\"globalBody\"]/oj-module/div[1]/div[2]/div/div/h1"));
 
-    
-    
-	WebElement cajaNombre = chrome.findElement(By.xpath("//*[@id=\"globalBody\"]/oj-module/div[1]/div[2]/div/div/div/input[1]"));
-	WebElement cajaEmail = chrome.findElement(By.xpath("//*[@id=\"globalBody\"]/oj-module/div[1]/div[2]/div/div/div/input[2]"));
-	WebElement cajaPwd1 = chrome.findElement(By.xpath("//*[@id=\"globalBody\"]/oj-module/div[1]/div[2]/div/div/div/input[3]"));
-	WebElement cajaPwd2 = chrome.findElement(By.xpath("//*[@id=\"globalBody\"]/oj-module/div[1]/div[2]/div/div/div/input[4]"));
-	//RemoteWebElement inputFile = (RemoteWebElement) chrome.findElement(By.xpath("//*[@id=\"globalBody\"]/oj-module/div[1]/div[2]/div/div/div/input[5]"));
-	
-	cajaNombre.sendKeys("selenium97");
-	cajaEmail.sendKeys("selenium97@gmail.com");
-	cajaPwd1.sendKeys("123");
-	cajaPwd2.sendKeys("123");
-	chrome.findElement(By.xpath("/html/body/div/oj-module/div[1]/div[2]/div/div/div/button")).click();
-	WebElement botonCrearCuenta = chrome.findElement(By.id("btnCrearCuenta"));
-	
-	chrome.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-assertThat(chrome.switchTo().alert().getText(), is("Registrado correctamente"));
-chrome.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-chrome.switchTo().alert().accept();
-    
-    
-    chrome.findElement(By.cssSelector("html")).click();
-    chrome.findElement(By.cssSelector(".oj-sm-12:nth-child(1) > input")).sendKeys("selenium1");
-    chrome.findElement(By.cssSelector("button")).click();
-  }
+	}
 }
