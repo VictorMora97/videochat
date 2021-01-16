7b304b781e7bc6e01f00b99330ac1dda4b14fe6c
VICTOR <CLASE@LAPTOP-HK1DJLBJ>
2021-01-16 02:26:59 +0100

    Guarda mensajes privados y su recipient

--- src/main/webapp/js/Conversacion.js
+++ src/main/webapp/js/Conversacion.js
@@ -20,5 +20,30 @@ class Conversacion {
 		this.chat.enviar(mensaje);
 		var mensaje = new Mensaje(this.textoAEnviar());
 		this.addMensaje(mensaje);
+		
+		
+		
+		var info = {
+				textoAEnviar : this.textoAEnviar(),
+				sender : "yoPrivado",
+				recipient : this.nombreInterlocutor
+		};
+		var data = {
+				data : JSON.stringify(info),
+				url : "messages/msgPrivado",
+				type : "put",
+				contentType : 'application/json',
+				success : function(response) {
+					//alert("Mensaje Broadcast guardado en BBDD");
+				},
+				error : function(response) {
+					alert("Error guardando msg en BD " + response.responseJSON.error);
+				}
+		};
+		$.ajax(data);
+		
+		
+		
+		
 	}
 }
