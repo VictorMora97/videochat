220aa609609abe0138817123ae3612b63c94e516
VICTOR <CLASE@LAPTOP-HK1DJLBJ>
2021-01-26 20:07:05 +0100

    Retoque mensajes privados (no afecta a defensa)

--- src/main/webapp/js/Mensaje.js
+++ src/main/webapp/js/Mensaje.js
@@ -2,5 +2,7 @@ class Mensaje {
 	constructor(texto, hora) {
 		this.texto = texto;
 		this.hora = hora ? hora : Date.now();
+		var fecha = new Date(Date.now());
+		this.horaFormateada = fecha.toLocaleString();
 	}
 }
